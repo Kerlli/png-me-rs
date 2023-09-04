927c382ee60e650b72c75c3c4c73476598675363
Kerlli <func.map0to1@gmail.com>
2023-09-04 16:49:44 +0800

    feat: add sub command `png-me set [file] [chunk_name] [message]`

--- cmd/src/cli/commands.rs
+++ cmd/src/cli/commands.rs
@@ -3,12 +3,20 @@ use clap::{Args, Subcommand};
 #[derive(Subcommand)]
 pub enum Commands {
   Info(InfoArgs),
+  Set(SetArgs),
   Remove(RemoveArgs),
 }
 
 #[derive(Args)]
 pub struct InfoArgs {
   pub file: String,
+}
+
+#[derive(Args)]
+pub struct SetArgs {
+  pub file: String,
+  pub chunk_name: String,
+  pub message: String,
 }
 
 #[derive(Args)]
--- cmd/src/main.rs
+++ cmd/src/main.rs
@@ -1,9 +1,11 @@
 mod cli;
 
+use std::str::FromStr;
 use cli::parse;
 use cli::commands::Commands;
 
 use png::Png;
+use png::chunk::{Chunk, ChunkType};
 use utils::fs::{read_file_buffer, write_buffer_to_file};
 
 fn main() {
@@ -19,6 +21,45 @@ fn main() {
         .expect("Not a valid png format");
       
       println!("{}", png.to_string());
+    },
+    Commands::Set(args) => {
+      let filepath = args.file;
+      let chunk_name = args.chunk_name;
+      let message = args.message;
+
+      let buffer = read_file_buffer(&filepath);
+
+      let mut png = Png::try_from(buffer.as_slice())
+        .expect("Not a valid png format");
+
+      let chunk = png.mut_chunk_by_type(&chunk_name);
+      
+      match chunk {
+        Some(c) => {
+          c.set_data(message.as_bytes());
+        },
+        None => {
+          let chunk_type = ChunkType::from_str(&chunk_name)
+            .expect("Invalid chunk type");
+          let data = message.into_bytes();
+          let chunk = Chunk::new(chunk_type, data);
+          // find IEND
+          let end_pos = png.chunk_position("IEND");
+
+          match end_pos {
+            Some(pos) => {
+              png.insert_chunk(pos, chunk);
+            },
+            None => {
+              eprintln!("Failed: can not found IEND chunk");
+              return;
+            }
+          }
+        },
+      };
+
+      write_buffer_to_file(&png.as_bytes()[..], &filepath);
+      println!("Success");
     },
     Commands::Remove(args) => {
       let filepath = args.file;
@@ -38,6 +79,6 @@ fn main() {
           println!("Failed to remove chunk {}: {}", chunk_name, e);
         },
       };
-    }
+    },
   };
 }
