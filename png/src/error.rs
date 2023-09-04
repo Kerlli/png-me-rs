29a0bb2111462c3be529834e5031f65f4af68e56
Kerlli <func.map0to1@gmail.com>
2023-09-04 13:38:05 +0800

    feat: add much more error types for `PngError`

--- png/src/error.rs
+++ png/src/error.rs
@@ -3,11 +3,18 @@ use std::fmt::Display;
 
 #[derive(Debug)]
 pub enum PngError {
+  InvalidBitDepth,
+  InvalidColorType,
+  InvalidCompressionMehtod,
+  InvalidFilterMethod,
+  InvalidFilterType,
   InvalidHeader,
+  InvalidInterlaceMethod,
   ChunkCrcMismatch,
   ChunkNotFoundError,
   ChunkParseError,
   ChunksIsEmptyError,
+  ChunkTypeParseError,
   IndexOutOfBounds,
 }
 
@@ -16,11 +23,18 @@ impl std::error::Error for PngError {}
 impl Display for PngError {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
     match self {
+      PngError::InvalidBitDepth => write!(f, "Invalid bit depth"),
+      PngError::InvalidColorType => write!(f, "Invalid color type"),
+      PngError::InvalidCompressionMehtod => write!(f, "Invalid compression method"),
+      PngError::InvalidFilterMethod => write!(f, "Invalid filter method"),
+      PngError::InvalidFilterType => write!(f, "Invalid filter type"),
       PngError::InvalidHeader => write!(f, "Invalid PNG header"),
+      PngError::InvalidInterlaceMethod => write!(f, "Invalid interlace method"),
       PngError::ChunkCrcMismatch => write!(f, "Chunk crc mismatch"),
       PngError::ChunkNotFoundError => write!(f, "Chunk not found"),
       PngError::ChunkParseError => write!(f, "Chunk parse error"),
       PngError::ChunksIsEmptyError => write!(f, "There're no chunks left"),
+      PngError::ChunkTypeParseError => write!(f, "Chunk type parse error"),
       PngError::IndexOutOfBounds => write!(f, "Index out of bounds"),
     }
   }
